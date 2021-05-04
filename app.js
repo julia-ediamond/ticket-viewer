60edcef1859ba7ba672c538d6acc0c4cc0afbdaf
iusharnina@gmail.com <zjulia679@gmail.com>
2021-05-04 14:01:24 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3007;
+
+applicationCache.addEventListener(PORT, () => {
+    console.log(`server is listening on ${PORT}`);
+});
